2e242a3ec7486605c9bb940d6e254d2d701656a6
devjun10 <devjun1023@gmail.com>
2023-09-29 22:23:51 +0900

    chore: project build

---
+++ rdb-lock/src/main/java/project/lock/rdblock/RdbLockApplication.java
@@ -0,0 +1,13 @@
+package project.lock.rdblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdbLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdbLockApplication.class, args);
+    }
+
+}
