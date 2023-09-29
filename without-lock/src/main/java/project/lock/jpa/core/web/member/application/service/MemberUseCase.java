28b7860c2be725ba427c799edacf0948e88a50bb
devjun10 <devjun1023@gmail.com>
2023-09-29 22:14:02 +0900

    refactor: change class name

--- without-lock/src/main/java/project/lock/jpa/core/web/member/application/service/MemberUseCase.java
+++ without-lock/src/main/java/project/lock/jpa/core/web/member/application/service/MemberUseCase.java
@@ -5,16 +5,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.lock.jpa.core.domain.member.entity.Member;
 import project.lock.jpa.core.domain.member.repository.MemberRepository;
+import project.lock.jpa.core.web.member.application.MemberSaveUseCase;
 import project.lock.jpa.core.web.member.application.MemberSearchUseCase;
 
 @Service
 @RequiredArgsConstructor
-public class MemberSearchService implements MemberSearchUseCase {
+public class MemberUseCase implements MemberSearchUseCase, MemberSaveUseCase {
 
     private final MemberRepository memberRepository;
 
     @Override
     public Optional<Member> findById(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    @Override
+    public void save(Member member) {
+        memberRepository.save(member);
     }
 }
