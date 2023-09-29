8c6cee76e27c085f012d695bf01a4ebfcfc315de
devjun10 <devjun1023@gmail.com>
2023-09-29 18:20:21 +0900

    chore: project build

---
+++ without-lock/src/main/java/project/lock/jpa/JpaLockApplication.java
@@ -0,0 +1,13 @@
+package project.lock.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLockApplication.class, args);
+    }
+
+}
